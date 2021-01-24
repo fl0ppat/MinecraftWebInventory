936716f1e60e4f04adc8521cc2d127e79a9cd6a8
Daniel Khrenov <klaven011@gmail.com>
2021-01-24 13:18:42 +0300

    feat: Create demo page

--- minecraftItems.js
+++ minecraftItems.js
@@ -1,6 +1,52 @@
-const minecraftItems = {
-  name: "Classic",
-  version: "1.7.10",
+const minecraftItems_1 = {
+  itemsList: [
+    {
+      itemName: "Diamond_Pickaxe",
+      displayName: "&fАлмазная кирка",
+      description: ["\n", "&9+5 к урону", "Прочность: 131/131"],
+    },
+  ],
+};
+const minecraftItems_2 = {
+  itemsList: [
+    {
+      itemName: "Cobblestone",
+      count: 32,
+      displayName: "&fБулыжник",
+    },
+    {
+      itemName: "Oak_Wood",
+      count: 16,
+      displayName: "&fДуб",
+    },
+    {
+      itemName: "Oak_Wood",
+      count: 16,
+      displayName: "&fДуб",
+    },
+    {
+      itemName: "Oak_Wood",
+      count: 16,
+      displayName: "&fДуб",
+    },
+    {
+      itemName: "Cobblestone",
+      count: 32,
+      displayName: "&fБулыжник",
+    },
+    {
+      itemName: "Cobblestone",
+      count: 32,
+      displayName: "&fБулыжник",
+    },
+    {
+      itemName: "Cobblestone",
+      count: 32,
+      displayName: "&fБулыжник",
+    },
+  ],
+};
+const minecraftItems_3 = {
   itemsList: [
     {
       itemName: "Stone_Sword",
@@ -15,67 +61,10 @@ const minecraftItems = {
     {
       itemName: "Stone_Pickaxe",
       displayName: "&fКаменная Кирка",
+      glow: true,
       description: ["\n", "&9+3 к урону", "Прочность: 131/131"],
     },
     {
-      itemName: "Stone_Axe",
-      displayName: "&fКаменный топор",
-      description: ["\n", "&9+4 к урону", "Прочность: 131/131"],
-    },
-    {
-      itemName: "Thaumonomicon",
-      displayName: "&2Штанишки",
-      count: 32,
-      description: ["&2строка1", "&2строка2", "&9+5 к урону", "Защита V", "", "Прочность: 100/100"],
-      shift: ["строка1", "строка2"],
-      mod: "&9MODMOD",
-    },
-    {
-      itemName: "Cobblestone",
-      count: 32,
-      displayName: "&fБулыжник",
-    },
-    {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
-    },
-    {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
-    },
-    {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
-    },
-    {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
-    },
-    {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
-    },
-    {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
-    },
-    {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
-    },
-    {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
-    },
-    {
       itemName: "Oak_Wood",
       count: 16,
       displayName: "&fДуб",
@@ -96,99 +85,19 @@ const minecraftItems = {
       displayName: "&fБулыжник",
     },
     {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
-    },
-    {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
-    },
-    {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
-    },
-    {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
-    },
-    {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
-    },
-    {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
-    },
-    {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
-    },
-    {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
-    },
-    {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
-    },
-    {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
-    },
-    {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
-    },
-    {
       itemName: "Cobblestone",
       count: 32,
       displayName: "&fБулыжник",
     },
     {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
-    },
-    {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
-    },
-    {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
-    },
-    {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
-    },
-    {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
-    },
-    {
-      itemName: "Oak_Wood",
-      count: 16,
-      displayName: "&fДуб",
+      itemName: "Cobblestone",
+      count: 32,
+      displayName: "&fБулыжник",
     },
   ],
 };
 
-export default minecraftItems;
+export { minecraftItems_1, minecraftItems_2, minecraftItems_3 };
 
 /*
 {
--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-import MinecraftWebInventory from "./MinecraftWebInventory.js";
-import minecraftItems from "./minecraftItems.js";
+import MinecraftWebInventory from "./src/MinecraftWebInventory.js";
+import { minecraftItems_1, minecraftItems_2, minecraftItems_3 } from "./minecraftItems.js";
 
 const config = {
   path: "/items_re",
 };
 
-const inv = new MinecraftWebInventory(minecraftItems, config);
-inv.createTable(document.querySelector(".table"));
+const case2_input = document.querySelector(".case-2__input");
+
+const inventory_1 = new MinecraftWebInventory(minecraftItems_1, config);
+inventory_1.createTable(document.querySelector("#case-1"));
+
+const inventory_2 = new MinecraftWebInventory(minecraftItems_2, config);
+inventory_2.createTable(document.querySelector("#case-2"));
+
+const inventory_3 = new MinecraftWebInventory(minecraftItems_3, config);
+inventory_3.createTable(document.querySelector("#case-3"));
+
+function rangeInputChangeSize() {
+  inventory_2.changeSize(this.parentNode.querySelector(".MinecraftWebInventory"), this.value);
+}
+
+case2_input.oninput = rangeInputChangeSize;
